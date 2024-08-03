2b8257841f2d46cba8236ce4406bf2aa33c89492
mevipinmaurya <mevipin2212001@gmail.com>
2024-08-03 23:38:13 +0530

    Creating TaskTrove, A ToDo app

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        {/* <Route path='/' element={} /> */}
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ Frontend/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <>
+            <div className="navbar bg-base-100 shadow-lg border-b-2 border-b-slate-800">
+                <a className="btn btn-ghost text-xl">TaskTrove</a>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
