849a7cbc9792a456b6c74ec94e894006f0eebaae
mevipinmaurya <mevipin2212001@gmail.com>
2024-08-07 00:18:51 +0530

    Frontend almost done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Body from './components/Body'
 import Footer from './components/Footer'
+import RegistrationPage from './components/RegistrationPage'
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path='/' element={<Body />} />
+        <Route path="/register" element={<RegistrationPage />} />
       </Routes>
       <Footer />
     </>
--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { IoMdLogOut } from "react-icons/io";
 
 const Navbar = () => {
     return (
         <>
             <div className="navbar bg-base-100 shadow-lg border-b-2 border-b-slate-800">
                 <a className="btn btn-ghost text-2xl hover:bg-transparent text-[#E34432]">TaskTrove</a>
+                <div className='flex justify-end w-full mr-5 text-3xl cursor-pointer'>
+                    <span className='hover:scale-125 text-red-700 tooltip tooltip-bottom' data-tip="Logout"><IoMdLogOut /></span>
+                </div>
             </div>
         </>
     )
